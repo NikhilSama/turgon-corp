185f7b06d3a79969b655a2260bdf16f153fc454e
Nikhil Sama <nikhilsama@gmail.com>
2024-03-11 18:41:10 +0530

    remove langchain core prompts

--- app/api/chain/route.ts
+++ app/api/chain/route.ts
@@ -1,45 +1,45 @@
 
 
-import {
-    LangChainStream,
-    StreamingTextResponse,
-    experimental_StreamData,
-  } from 'ai';
-  import { LLMChain } from 'langchain/chains';
-  import { OpenAI } from '@langchain/openai';
-//   import { PromptTemplate } from '@langchain/core/prompts';
+// import {
+//     LangChainStream,
+//     StreamingTextResponse,
+//     experimental_StreamData,
+//   } from 'ai';
+//   import { LLMChain } from 'langchain/chains';
+//   import { OpenAI } from '@langchain/openai';
+// //   import { PromptTemplate } from '@langchain/core/prompts';
   
-  export const runtime = 'edge';
+//   export const runtime = 'edge';
   
-  export async function POST(req: Request) {
-    const { prompt: value } = await req.json();
-    console.log("product is: ", { prompt: value});
+//   export async function POST(req: Request) {
+//     const { prompt: value } = await req.json();
+//     console.log("product is: ", { prompt: value});
   
-    const model = new OpenAI({
-      modelName: "gpt-3.5-turbo",    
-      temperature: 0,
-      streaming: true
-    });
+//     const model = new OpenAI({
+//       modelName: "gpt-3.5-turbo",    
+//       temperature: 0,
+//       streaming: true
+//     });
   
-    const prompt = PromptTemplate.fromTemplate(
-      'What is a good name for a company that makes {product}?',
-    );
+//     const prompt = PromptTemplate.fromTemplate(
+//       'What is a good name for a company that makes {product}?',
+//     );
   
-    const chain = new LLMChain({ llm: model, prompt });
+//     const chain = new LLMChain({ llm: model, prompt });
   
-    const data = new experimental_StreamData();
+//     const data = new experimental_StreamData();
   
-    // important: use LangChainStream from the AI SDK:
-    const { stream, handlers } = LangChainStream({
-      onFinal: () => {
-        data.append(JSON.stringify({ key: 'value' })); // example
-        data.close();
-      },
-      experimental_streamData: true,
-    });
+//     // important: use LangChainStream from the AI SDK:
+//     const { stream, handlers } = LangChainStream({
+//       onFinal: () => {
+//         data.append(JSON.stringify({ key: 'value' })); // example
+//         data.close();
+//       },
+//       experimental_streamData: true,
+//     });
   
-    await chain.stream({ product: value }, { callbacks: [handlers] });
+//     await chain.stream({ product: value }, { callbacks: [handlers] });
   
-    return new StreamingTextResponse(stream, {}, data);
-  }
+//     return new StreamingTextResponse(stream, {}, data);
+//   }
   
