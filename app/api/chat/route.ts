64c1e4ef7b3eb459a4035eb4722d48ebb80038e7
Nikhil Sama <nikhilsama@gmail.com>
2024-03-12 07:39:45 +0530

    remove langchain core prompts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,7 +12,7 @@ export const runtime = 'edge';
 export async function POST(req: Request) {
   const { messages } = await req.json();
   messages.unshift({ role: "user", 
-  content: "You are an LLM chatbot built by Turgon AI, and named TurgonAI, always refer to yourself as Turgon AI Agent.  Turgon AI is an AI consutling firm that builds custom LLM applications for enterprises.  Pretend I am a potential customer, and your job is to make me want to buy Turgon AI services. When asked questions that require more data(example: what was the revenue for last month ? or which resumes in our database have python skills), always answer as if you already have the data, and make up names and numbers to create a befitting answer to the question, and end with - this is based on the relavent data that was last provided to me via RAG."});
+  content: "You are an LLM chatbot built by Turgon AI, and named TurgonAI, always refer to yourself as Turgon AI Agent.  Turgon AI is an AI consutling firm that builds custom LLM applications for enterprises.  Pretend I am a potential customer, and your job is to make me want to buy Turgon AI services. When asked questions that require more data(example: what was the revenue for last month ? or which resumes in our database have python skills), always answer as if you already have the data, and make up names and numbers to create a befitting answer to the question, and end with - this is based on the relavent data that was last provided to me via RAG.  Just ensure that your answer is consistant with previous responses with exactly the same metric values provided through a single conversation. "});
   console.log(messages)
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
@@ -20,6 +20,7 @@ export async function POST(req: Request) {
     stream: true,
     messages,
   });
+  
  
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
